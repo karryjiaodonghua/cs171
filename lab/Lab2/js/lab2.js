8b27eb029b5d6277714c9cf40e5bfc94bd9cb3b3
Patrick Pan <patrick.pan@patrickpan.com>
2016-02-04 15:47:49 -0500

    Merged halves

--- lab/Lab2/js/lab2.js
+++ lab/Lab2/js/lab2.js
@@ -5,7 +5,7 @@ dataFiltering();
 function dataFiltering(type) {
 	var attractions = attractionData;
 
-  if(type != "All Attractions" && typeof type != "undefined") {
+  if(type != "all" && typeof type != "undefined") {
     attractions = attractions.filter((attraction) => {
       return attraction.Category === type;
     })
@@ -16,25 +16,8 @@ function dataFiltering(type) {
   }).slice(0,5);
 
   renderBarChart(topFiveVisitors(attractions).slice(0,5))
-
-	/* **************************************************
-	 *
-	 * ADD YOUR CODE HERE (ARRAY/DATA MANIPULATION)
-	 *
-	 * CALL THE FOLLOWING FUNCTION TO RENDER THE BAR-CHART:
-	 *
-	 * renderBarChart(data)
-	 *
-	 * - 'data' must be an array of JSON objects
-	 * - the max. length of 'data' is 5
-	 *
-	 * **************************************************/
-
 }
 
-
-
 document.getElementById("attraction-category").addEventListener("change", (e) => {
-  var type = e.target.value;
-  dataFiltering(type)
+  dataFiltering(e.target.value);
 })
