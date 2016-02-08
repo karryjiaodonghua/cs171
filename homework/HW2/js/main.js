333bea73a42a6437ad39cea8b77b969d95f6bc3c
Patrick Pan <patrick.pan@patrickpan.com>
2016-02-08 10:52:04 -0500

    Finished code for HW 2

--- homework/HW2/js/main.js
+++ homework/HW2/js/main.js
@@ -9,15 +9,34 @@
 
 // FILTER DATA, THEN DISPLAY SUMMARY OF DATA & BAR CHART
 
+const sourceDeliveryData = deliveryData;
+const sourceFeedbackData = feedbackData;
+
 createVisualization();
 
-function addStat (name, value) {
+function addStat(name, value) {
   $("#stats").append(
     "<tr>" +
-      "<td>" + name + "</td>" +
-      "<td>" + value + "</td>" +
+    "<td>" + name + "</td>" +
+    "<td>" + value + "</td>" +
     "</tr>"
   );
+}
+
+function clearStats() {
+  $("#stats tr").not(":first").remove();
+}
+
+$("#area").change(createVisualization);
+$("#type").change(createVisualization);
+
+function filterData(data, options) {
+  Object.keys(options).forEach((key) => {
+    console.log(key)
+    console.log(options[key])
+    data = data.filter((entry) => typeof entry[key] !== "undefined" && entry[key].toUpperCase() == options[key].toUpperCase());
+  })
+  return data;
 }
 
 function createVisualization() {
@@ -36,39 +55,47 @@ function createVisualization() {
    * 4) React to user input and start with (1)
    *
    * ************************************************************/
+  var area = $("#area").val();
+  var type = $("#type").val();
+  var filter = {};
+  if (area != "all") filter.area = area;
+  if (type != "all") filter.order_type = type;
 
+  filteredDeliveryData = filterData(sourceDeliveryData, filter);
+  filteredFeedbackData = filterData(sourceFeedbackData, filter);
 
-  var numDeliveries = deliveryData.length;
-  var totalDelivered = deliveryData.reduce((total, next) => {
+  var numDeliveries = filteredDeliveryData.length;
+  var totalDelivered = filteredDeliveryData.reduce((total, next) => {
     return {
       count: total.count + next.count
     }
   }).count;
-  var avgTime = deliveryData.reduce((total, next) => {
+  var avgTime = filteredDeliveryData.reduce((total, next) => {
     return {
       delivery_time: total.delivery_time + next.delivery_time
     }
   }).delivery_time / numDeliveries;
-  var totalSales = deliveryData.reduce((total, next) => {
+  var totalSales = filteredDeliveryData.reduce((total, next) => {
     return {
       price: total.price + next.price
     }
   }).price;
-  var numFeedback = feedbackData.length;
+  var numFeedback = filteredFeedbackData.length;
   var feedbackByType = {
-    high: feedbackData.filter((f) => f.quality === "high").length,
-    medium: feedbackData.filter((f) => f.quality === "medium").length,
-    low: feedbackData.filter((f) => f.quality === "low").length
+    high: filteredFeedbackData.filter((f) => f.quality === "high").length,
+    medium: filteredFeedbackData.filter((f) => f.quality === "medium").length,
+    low: filteredFeedbackData.filter((f) => f.quality === "low").length
   }
 
-  addStat("Number of deliveries: ", numDeliveries);
-  addStat("Number of pizzas delivered: ", totalDelivered);
-  addStat("Average delivery time: ", "$" + avgTime.toFixed(2));
-  addStat("Total Sales: ", totalSales);
-  addStat("Number of feedback: ", numFeedback);
+  clearStats();
+  addStat("Number of deliveries", numDeliveries);
+  addStat("Number of pizzas delivered", totalDelivered);
+  addStat("Average delivery time (minutes)", avgTime.toFixed(0) + "");
+  addStat("Total Sales (USD)", "$" + totalSales.toFixed(2));
+  addStat("Number of feedback", numFeedback);
   addStat("High rating", feedbackByType.high);
   addStat("Medium rating", feedbackByType.medium);
   addStat("Low rating", feedbackByType.low);
 
-  renderBarChart(deliveryData)
+  renderBarChart(filteredDeliveryData)
 }
